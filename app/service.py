0ad26ae13e21cce20e17e7a8a3fd32eb613c5b47
mohcine.rouessi <mohcine.rouessi@bp.com>
2022-06-01 23:00:00 +0100

    api app to fetch data

--- app/service.py
+++ app/service.py
@@ -1,112 +1,29 @@
 import json
 from typing import List, Dict
 from pydantic import ValidationError
-from app.utils import UserEvent, OrgEvent, UserResponse, APP_DIR_ABS_PATH
-
+from app.utils import UserLastState, UserResponse, APP_DIR_ABS_PATH
+from redis_om import Migrator
 
 
 class UsersService:
 
 
-    USER_EVENTS_FILE = f"{APP_DIR_ABS_PATH}/data/user_events.json"
-    ORG_EVENTS_FILE  = f"{APP_DIR_ABS_PATH}/data/org_events.json"
-
-    
-    def read_user_events_file(self) -> List[Dict[str, str]]:
+    def _get_user_by_username(username: str) -> UserLastState:
         """
-        Description: Read user events file
-        Return: 
-            (List[Dict[str, str]]): list of user events dictionaries
+        Description: get the last saved state for a user based on the user id
+        Params:
+            username (str): the user id
+        Returns:
+            (UserLastState): the user last saved state if found 
         """
-        with open(self.USER_EVENTS_FILE) as f:
-            return json.load(f)
-
-
-    def read_org_events_file(self) -> List[Dict[str, str]]:
-        """
-        Description: Read organisation events file
-        Return: 
-            (List[Dict[str, str]]): list of organisation events dictionaries
-        """
-        with open(self.ORG_EVENTS_FILE) as f:
-            return json.load(f)
-
-
-    def parse_user_events(self, user_events: List[Dict[str, str]]) -> List[UserEvent]:
-        """
-        Description: Parse user events from file to UserEvent Objects, and ignore malformed events
-        Args: 
-            user_events (List[Dict[str, str]]): list of user events from file
-        Return:
-            (List[UserEvent]) : list of well formed user events objects
-        """
-        user_events_objects = []
-        for user_event in user_events:
-            try:
-                user_event = UserEvent(**user_event)
-                user_events_objects.append(user_event)
-            except ValidationError as e:
-                continue
-                #print("MALFORMED USER EVENT IGNORED : ", user_event)
-        return user_events_objects
-
-
-    def parse_org_events(self, org_events: List[Dict[str, str]]) -> List[OrgEvent]:
-        """
-        Description: Parse organisation events from file to orgEvent Objects, and ignore malformed events
-        Args: 
-            org_events (List[Dict[str, str]]): list of organisation events from file
-        Return:
-            (List[orgEvent]) : list of well formed org events objects
-        """
-        org_events_objects = []
-        for org_event in org_events:
-            try:
-                org_event = OrgEvent(**org_event)
-                org_events_objects.append(org_event)
-            except ValidationError as e:
-                continue
-                #print("MALFORMED org EVENT IGNORED : ", org_event)
-        return org_events_objects
-
-
-    def get_user_last_state(self, user_name: str) -> UserEvent:
-        """
-        Description: Fetch the last state for the provided user_name
-        Args: 
-            user_name (str): username
-        Return:
-            (UserEvent): UserEvent object that represents the user last state
-        """
-        users_from_file = self.read_user_events_file(self)
-        users = self.parse_user_events(self, users_from_file)
-        user_last_state = None
-        last_state_date = ""
-        for user in users:
-            if user.username.lower() == user_name.lower() and user.received_at > last_state_date:
-                user_last_state = user
-                last_state_date = user_last_state.received_at
-        return user_last_state
-
-
-    def get_organisation_tier(self, organization_name: str) -> str:
-        """
-        Description: Fetch Organisation tier for the provided organisation name
-        Args:
-            organisation_name (str): organisation name
-        Return:
-            (str): organisation tier
-        """
-        if not organization_name:
+        try:
+            user = UserLastState.find(UserLastState.username == username).all()
+            if user:
+                return user[0]
+        except Exception as e: 
+            print(f"UNFOUND USER ID : {username} ", e)
             return None
 
-        orgs_from_file = self.read_org_events_file(self)
-        orgs = self.parse_org_events(self, orgs_from_file)
-        org_tier = None
-        for org in orgs:
-            if org.organization_name == organization_name and org.organization_tier:
-                org_tier = org.organization_tier
-        return org_tier
 
 
     @classmethod
@@ -118,23 +35,16 @@ class UsersService:
         Return:
             (UserRespone): the user last state with organisation informations
         """
-        user_last_state = self.get_user_last_state(
-            self = self,
-            user_name = user_name
-        )
+        user_last_state = self._get_user_by_username(user_name)
         
         if not user_last_state:
             return None
-
-        user_org_tier = self.get_organisation_tier(
-            self = self,
-            organization_name = user_last_state.organization_name
-        )
+            
         user_last_state_enriched = UserResponse(
             username          = user_last_state.username,
             user_type         = user_last_state.user_type,
             organization_name = user_last_state.organization_name,
-            organization_tier = user_org_tier
+            organization_tier = user_last_state.organization_tier
         )
         return user_last_state_enriched
 
--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, ValidationError, validator
 from typing import Optional
+from redis_om import HashModel, Field
 import os
 
 
@@ -7,21 +8,14 @@ class UserValidationException(Exception):
     """USER DATA VALIDATION"""
 
 
-class UserEvent(BaseModel):
-    id: str 
-    event_type: str 
-    username: str 
-    user_email: Optional[str] 
-    user_type: str 
-    organization_name: str 
-    received_at: str 
-
-
-class OrgEvent(BaseModel):
-    organization_key: str
-    organization_name: str
+class UserLastState(HashModel):
+    user_id: str = Field(index=True)
+    username: str = Field(index=True)
+    user_type: Optional[str]
+    organization_key: Optional[str] 
+    organization_name: Optional[str] = Field(index=True)
     organization_tier: Optional[str]
-    created_at: str
+    received_at: str 
 
 
 class UserResponse(BaseModel):
@@ -36,14 +30,5 @@ APP_DIR_ABS_PATH = os.path.abspath(os.path.dirname(__file__))
 
 
 if __name__ == "__main__":
-    a = UserEvent(
-        id="", 
-        event_type="EVENT TYPE str", 
-        username="USER NAME", 
-        user_email="USER EMAIL str", 
-        user_type="USER TYPE str", 
-        organization_name="ORG NAME str", 
-        received_at="RECEIVED AT str"
-    )
-    print(a.id)
+   pass
 
